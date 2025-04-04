f3f2c9202054290138cdaffe546b39a54fffbe93
Will Field <wfield@gc.cuny.edu>
2025-04-04 10:31:06 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # maplibre-gl-map-to-image
-A plugin for maplibregljs that captures a map (along with marker, popups, and controls if desired) and converts it to a raster image.
+
+A JavaScript library that generates an HTML image element with a PNG source from a Maplibre GL JS map. The library allows users to customize the map image by selecting which overlays to include or exclude, such as markers, popups, and controls. It also allows the map to fit a bounding box only for the purpose of image generation.
+
+## Features
+
+* Generate a PNG image from a Maplibre GL JS map
+* Customize the image by selecting which overlays to include:
+	+ Markers
+	+ Popups
+	+ Controls
+* Render overlays outside of the main Maplibre WebGL canvas context
+* Set the source of an existing HTML `img` element to the generated PNG data
+
+## Future Development
+
+* Support for generating images in formats other than PNG
+* Direct access to image data for use cases like direct downloading
+* Support different image resolutions and aspect ratios that may not match the current map canvas
+
+## Dependencies
+
+* [htmlToImage](https://www.npmjs.com/package/html-to-image)
+
+## Usage
+
+To use this library, simply call the `toPng` function and pass in your Maplibre GL JS map instance, along with options for which overlays to include. The function will return a PNG image that can be set as the source of an existing HTML `img` element.
+
+```javascript
+import { toPng } from 'maplibre-image-generator';
+
+const map = new maplibregl.Map({
+  // your map options here
+});
+
+const options = {
+    targetImageId, //* @param {string} REQUIRED: The ID of the target image element where the PNG will be set.
+    [bbox], //* @param {array} Optional bounding box to fit the map before conversion.
+    hideAllControls, //* @param {boolean} Flag to hide all map controls during conversion.
+    [hideControlsInCorner], //* @param {array} Specific corners to hide controls from.
+    hideMarkers, //* @param {boolean} Flag to hide markers during conversion.
+    hidePopups, //* @param {boolean} Flag to hide popups during conversion.
+    [hideVisibleLayers], //* @param {array}Layer IDs to hide during conversion.
+    [showHiddenLayers], //* @param {array} Layer IDs to show during conversion.
+}
+
+const image = await toPng(map, options);
+
+const imgElement = document.getElementById('my-image');
+imgElement.src = image;
+```
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import * as htmlToImage from 'html-to-image';
+/**
+ * Converts a MapLibre map to a PNG image and sets the image source to a specified target element.
+ *
+ * @param {object} map - The MapLibre map instance.
+ * @param {object} options - Configuration options for the conversion process.
+ * @param {string} options.targetImageId - REQUIRED: The ID of the target image element where the PNG will be set.
+ * @param {array} [options.bbox] - Optional bounding box to fit the map before conversion.
+ * @param {boolean} [options.hideAllControls] - Flag to hide all map controls during conversion.
+ * @param {array} [options.hideControlsInCorner] - Specific corners to hide controls from.
+ * @param {boolean} [options.hideMarkers] - Flag to hide markers during conversion.
+ * @param {boolean} [options.hidePopups] - Flag to hide popups during conversion.
+ * @param {array} [options.hideVisibleLayers] - Layer IDs to hide during conversion.
+ * @param {array} [options.showHiddenLayers] - Layer IDs to show during conversion.
+ * @returns {Promise} Resolves when the image has been successfully generated and set.
+ */
+
 export async function toPng(map, options) {
     let originalBounds = map.getBounds();
     if (options.bbox) {
         map.fitBounds(options.bbox, { animate: false });
     }
-    
+
     const mapElement = map.getContainer();
     mapElement.style.height = `${mapElement.getBoundingClientRect().height}px`;
     const targetImageElement = document.getElementById(options.targetImageId);
@@ -51,6 +67,21 @@ export async function toPng(map, options) {
     })
 }
 
+/**
+ * Manages the visibility of various overlay elements on a virtual map clone based on provided options.
+ *
+ * @param {object} map - The MapLibre map instance.
+ * @param {object} options - Configuration options that dictate which elements to hide or show.
+ * @param {boolean} [options.hideAllControls] - Flag to hide all map controls.
+ * @param {array} [options.hideControlsInCorner] - Specific corners from which to hide controls.
+ * @param {boolean} [options.hideMarkers] - Flag to hide all map markers.
+ * @param {boolean} [options.hidePopups] - Flag to hide all map popups.
+ * @param {array} [options.hideVisibleLayers] - Layer IDs to hide on the map by setting their visibility to 'none'.
+ * @param {array} [options.showHiddenLayers] - Layer IDs to show on the map by setting their visibility to 'visible'.
+ * @param {array} [options.bbox] - Optional bounding box to fit the map to, with padding.
+ * @param {HTMLElement} virtualClone - The cloned map container where the visibility changes are applied.
+ */
+
 function manageOverlayVisibility(map, options, virtualClone) {
     if (options.hideAllControls) {
         removeElements(virtualClone, '.maplibregl-control-container');
@@ -95,10 +126,23 @@ function manageOverlayVisibility(map, options, virtualClone) {
     }
 }
 
+/**
+ * Removes all DOM elements matching the specified selector within a given parent element.
+ *
+ * @param {HTMLElement} element - The parent element containing elements to be removed.
+ * @param {string} selector - The CSS selector to match elements that should be removed.
+ */
+
 function removeElements(element, selector) {
     Array.from(element.querySelectorAll(selector)).forEach((el) => el.remove());
 }
 
+/**
+ * Creates a new canvas element and copies the content of the given canvas element to it.
+ *
+ * @param {HTMLElement} canvas - The canvas element to be copied.
+ * @returns {HTMLElement} A new canvas element with the same dimensions and content as the original.
+ */
 function copyCanvas(canvas) {
 
     const newCanvas = document.createElement('canvas');
