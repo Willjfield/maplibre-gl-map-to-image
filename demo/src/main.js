fab676807c6ae9b69d0720c4fc51166e14891ef8
Will Field <wfield@gc.cuny.edu>
2025-04-28 12:12:39 -0400

    maplibre

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,5 +1,7 @@
 import {toElement} from '../../index.js';
 import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 const map = new maplibregl.Map({
     container: 'map',
     style: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
